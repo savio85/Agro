06ec2c805a708c8cafc690c06daa280280d3eceb
Savio Junior <saviojunio566@GMAIL.COM>
2025-12-16 16:28:22 -0300

    andamento do menu hamburguer

---
+++ navbar.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector('.botao-menu')
+const menuLateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const background = document.querySelector('.background')
+
+botao.addEventListener('click', () => {
+        menuLteral.classList
+})
