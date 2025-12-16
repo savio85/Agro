7a1aba49f89971fd3bffa84e266dae734268d3ae
Savio Junior <saviojunio566@GMAIL.COM>
2025-12-16 16:57:20 -0300

    menu finalizado

--- navbar.js
+++ navbar.js
@@ -4,5 +4,17 @@ const conteudo = document.querySelector('.conteudo')
 const background = document.querySelector('.background')
 
 botao.addEventListener('click', () => {
-        menuLteral.classList
+        menuLateral.classList.toggle('ativo')
+        conteudo.classList.toggle('ativo')
+        botao.classList.toggle('ativo')
+        background.classList.toggle('ativo')
+        document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
+})
+
+background.addEventListener('click', () => {
+        menuLateral.classList.remove('ativo')
+        conteudo.classList.remove('ativo')
+        botao.classList.remove('ativo')
+        background.classList.remove('ativo')
+        document.body.style.backgroundColor = '#ecf0f1'
 })
